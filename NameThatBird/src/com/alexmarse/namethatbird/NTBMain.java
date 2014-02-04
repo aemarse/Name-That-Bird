02e59093c42b1b598eb506a5a860397f5f5d4da4
Alex Marse <aemarse@gmail.com>
2014-02-04 16:21:40 -0500

    Added a bunch more Activities, started shelling out the UI

---
+++ NameThatBird/src/com/alexmarse/namethatbird/NTBMain.java
@@ -0,0 +1,55 @@
+package com.alexmarse.namethatbird;
+
+import com.alexmarse.namethatbird.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class NTBMain extends Activity {
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ntbmain);
+    }
+
+    // BUTTON CLICK HANDLER
+    public void onButtonClick(View v) {
+    	
+    	// Figure out which button was clicked and launch the proper activity
+    	switch(v.getId()) {
+    	case R.id.b_listen:
+    		
+    		// Set up the Intent
+    		Intent intent = new Intent(this, NTBSelectPlaylistType.class);
+    		
+    		// Start the SelectPlaylistType Activity
+        	startActivity(intent);
+        	
+    		break;
+    	case R.id.b_record:
+    		
+    		// Set up the Intent
+    		
+    		
+    		// Start the RecordAudio Activity
+    		
+    		
+    		break;
+    	default:
+    		break;
+    	}
+    }
+    
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.ntbmain, menu);
+        return true;
+    }
+    
+}
---
+++ NameThatBird/src/com/alexmarse/namethatbird/NTBSelectPlaylistType.java
@@ -0,0 +1,94 @@
+package com.alexmarse.namethatbird;
+
+import com.alexmarse.namethatbird.R;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class NTBSelectPlaylistType extends Activity {
+
+	@SuppressLint("NewApi")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ntbselect_playlist_type);
+		// Show the Up button in the action bar.
+		setupActionBar();
+		
+		Intent intent = getIntent();
+		Log.e("onCreate", "NTBSelectPlaylist Activity launched!");
+		
+	}
+
+	// BUTTON CLICK HANDLER
+	public void onButtonClick(View v) {
+    	
+    	// Figure out which button was clicked and launch the proper activity
+    	switch(v.getId()) {
+    	case R.id.b_location:
+    		
+    		// Set up the Intent
+    		Intent intent = new Intent(this, NTBSelectLocation.class);
+    		
+    		// Start the SelectLocation Activity
+        	startActivity(intent);
+        	
+    		break;
+    	case R.id.b_species:
+    		
+    		// Set up the Intent
+    		
+    		
+    		// Start the SelectSpecies Activity
+    		
+    		
+    		break;
+    	default:
+    		break;
+    	}
+    }
+	
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.ntbselect_playlist_type, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
